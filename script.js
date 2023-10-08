2468ee3089398053f2e6d6804c1a3de4fe811884
Muhammad Hany <dev.mhany@gmail.com>
2023-10-09 02:27:39 +0300

    DoneSendingToWhatsapp

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
     const govSelect = document.querySelector('select[name="government"]');
-    const productTypeSelect = document.querySelector('select[name="productType"]');
+    const productTypeCheckboxes = document.querySelectorAll('input[name="productType"]');
     const transPriceSpan = document.getElementById('trans');
     const currentPriceSpan = document.getElementById('total-all');
     const totalSpan = document.getElementById('total');
     const quantityInput = document.querySelector('input[name="quantity"]');
+    const productQuantities = document.querySelectorAll('.product-quantity');
 
     function updateTotal() {
         const transPrice = parseFloat(transPriceSpan.textContent) || 0;
         const currentPrice = parseFloat(currentPriceSpan.textContent) || 0;
         totalSpan.textContent = transPrice + currentPrice;
     }
-
+    productQuantities.forEach(quantityInput => {
+        quantityInput.addEventListener('input', updateCurrentPrice);
+    });
     function updateCurrentPrice() {
-        const quantity = parseInt(quantityInput.value) || 1;
-
-        let productPrice = 0;
-
-        switch (productTypeSelect.value) {
-            case "PowerBank":
-                productPrice = 150;
-                break;
-            case "Charger":
-                productPrice = 200;
-                break;
-            // Add more cases for additional product types as needed
-        }
-        currentPriceSpan.textContent = productPrice * quantity;
+        let totalProductPrice = 0;
+        
+        productTypeCheckboxes.forEach((checkbox, index) => {
+            if (checkbox.checked) {
+                const productPrice = parseFloat(checkbox.getAttribute('data-price')) || 0;
+                const productQuantity = productQuantities[index].value ? parseInt(productQuantities[index].value) : 1;
+                totalProductPrice += productPrice * productQuantity;
+            }
+        });
+        let totalQuantity = 0;
+        productQuantities.forEach(input => {
+            totalQuantity += parseInt(input.value) || 0;  // The '|| 0' is to handle NaN in case the input is empty
+        });
+        
+        currentPriceSpan.textContent = totalProductPrice;
+        updateTotal();
     }
+
+    productTypeCheckboxes.forEach((checkbox, index) => {
+        checkbox.addEventListener('change', function() {
+            if (this.checked) {
+                productQuantities[index].disabled = false;
+            } else {
+                productQuantities[index].disabled = true;
+                productQuantities[index].value = '';
+            }
+            updateCurrentPrice();
+        });
+    });
+
     govSelect.addEventListener('change', function() {
         let transPrice = 0;
         switch (this.value) {
@@ -66,17 +84,49 @@ document.addEventListener('DOMContentLoaded', function() {
         updateTotal();
     });
 
-    productTypeSelect.addEventListener('change', function() {
-        updateCurrentPrice();
-        updateTotal();
-    });
-
-    quantityInput.addEventListener('input', function() {
-        updateCurrentPrice();
-        updateTotal();
-    });
-
-    // Initial updates for when the page loads
-    updateCurrentPrice();
-    updateTotal();
+    quantityInput.addEventListener('input', updateCurrentPrice);
 });
+
+function sendToWhatsApp() {
+    const name = document.querySelector('input[name="name"]').value;
+    const government = document.getElementById('gov').value;
+    const selectedProductsCheckboxes = document.querySelectorAll('input[name="productType"]:checked');
+    const productQuantities = document.querySelectorAll('.product-quantity');
+
+    let selectedProductsDetails = [];
+    let quantities = []; // array to hold the quantities of each selected product
+    selectedProductsCheckboxes.forEach((checkbox, index) => {
+        const productName = checkbox.nextElementSibling.textContent;
+        const productPrice = parseFloat(checkbox.getAttribute('data-price'));
+        const productQuantity = productQuantities[index].value || 1;
+        const productTotal = productPrice * productQuantity;
+        selectedProductsDetails.push(`${productName} (Quantity: ${productQuantity}) - ${productTotal}`);
+        quantities.push(productQuantity); // add each product's quantity to the array
+    });
+
+    const totalQuantity = quantities.join(', '); // join quantities with a comma
+
+    const address = document.querySelector('input[name="address"]').value;
+    const phone = document.querySelector('input[name="phone"]').value;
+    const messageText = document.querySelector('input[name="message"]').value;
+    const total = document.getElementById('total').textContent;
+    const trans = document.getElementById('trans').textContent;
+    const totalAll = document.getElementById('total-all').textContent;
+
+    const message = `
+    Name: ${name}
+    Governorate: ${government}
+    Selected Products: 
+    ${selectedProductsDetails.join('\n')}
+    Address: ${address}
+    Phone: ${phone}
+    Total Quantity: ${totalQuantity}
+    Notes: ${messageText}
+    Total Price: ${total}
+    Shipping Fees: ${trans}
+    Current Price: ${totalAll}
+    `;
+
+    const encodedMessage = encodeURIComponent(message);
+    window.open(`https://wa.me/+201029384721?text=${encodedMessage}`, '_blank');
+}
